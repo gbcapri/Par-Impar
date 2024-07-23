72ee72e49aa60dd21b223d2e1f2bf40a672497a6
gbcapri <gbcapri@gmail.com>
2024-07-23 08:41:46 -0300

    init: first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+const modulo = require("./modulo.js");
+
+let num;
+
+do {
+    num = prompt("Digite um número: ").trim();
+} while (!modulo.verificaNum(num));
+
+let somaAlgarismos = modulo.somaAlgarismos(num);
+modulo.verificaPar(somaAlgarismos);
